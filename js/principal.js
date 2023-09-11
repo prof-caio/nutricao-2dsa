6e43d126fd926aff551ee21a5afdf2212324d5df
prof-caio <126119206+prof-caio@users.noreply.github.com>
2023-09-11 11:40:12 -0300

    Add files via upload

---
+++ js/principal.js
@@ -0,0 +1,13 @@
+//console.log("O arquivo separado está funcionando!");
+
+//-----------------Daqui para baixo começa o código----
+
+//alteração do título
+//console.log(document);
+//cria a variável titulo e armazena o conteudo da classe titulo nela
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na variável titulo
+titulo.textContent = "Fake Natty Nutrição!";
+
+//alteração da tabela
